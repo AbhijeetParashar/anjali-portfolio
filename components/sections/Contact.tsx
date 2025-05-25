ff615c304b41c034e8439b1a0f83c47377354c36
Abhijeet Kumar <iamabhijeet.29@gmail.com>
2025-05-25 21:07:30 +0400

    For Changing the env file

--- components/sections/Contact.tsx
+++ components/sections/Contact.tsx
@@ -46,10 +46,10 @@ export default function Contact() {
     if (formRef.current) {
       try {
         await emailjs.sendForm(
-          "service_rf66mi1",
-          "template_cy5ngpi",
+          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
           formRef.current,
-          "jyERb-aqsfASvhpUw"
+          process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
         );
 
         toast({
